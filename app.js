21d3cca8d0a4ea52430d41a18d3e1571ed000b95
Manoj Sreekumar <53040187+ManojSreekumarK@users.noreply.github.com>
2023-03-24 17:24:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,10 +65,11 @@ const app = Vue.createApp({
       this.hn = Math.floor(Math.random() * 6) + 5;
      if (this.playerHealth <= 90) {
         this.playerHealth += this.hn;
+       this.logitems.push(`Player used heal player health + ${this.hn}`);
       } else {
         alert("High health");
       }
-      this.logitems.push(`Player used heal player health + ${this.hn}`);
+      
       this.isButtonDisabled = true;
       this.remainingTime = 8;
       const updateTimerId = setInterval(() => {
